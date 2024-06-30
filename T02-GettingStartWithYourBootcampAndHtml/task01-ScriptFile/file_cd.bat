3947b3ba15ad86e45e3a7d043484a543f57731df
NodokaHirata <nodokahirata@gmail.com>
2024-06-30 23:26:27 +0100

    Task02 PT01 script file

---
+++ T02-GettingStartWithYourBootcampAndHtml/task01-ScriptFile/file_cd.bat
@@ -0,0 +1,8 @@
+rem create 3 new folders
+mkdir TO2_task1_01 TO2_task1_02 TO2_task1_03
+rem move to TO2_task01 directory
+cd TO2_task01
+rem create 3 new folders
+mkdir TO2_task1_04 TO2_task1_05 TO2_task1_06
+rem remove 2 folders
+rmdir TO2_task1_06 TO2_task01_05
